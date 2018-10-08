7e1bc25ad4f07f1d0732a99fa0c1fab4d34190a4
baumertjonathan <bau14003@byui.edu>
2018-10-08 13:06:53 -0600

    added shopping cart

---
+++ web/shoppingcart.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<head>
+	<title>Shopping Page</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+
+<ul>
+	<li><a href=https://mighty-journey-34749.herokuapp.com/hello.html>Home Page</a></li>
+	<li><a href=https://mighty-journey-34749.herokuapp.com/hello.html>Assignemnt 2 coming soon!</a></li>
+	<li><a href=https://mighty-journey-34749.herokuapp.com/hello.html>...</a></li>
+	<li><a href=https://mighty-journey-34749.herokuapp.com/hello.html>...</a></li>
+	<li><a href=https://mighty-journey-34749.herokuapp.com/hello.html>...</a></li>
+</ul>
+
+<div style="margin-left:25%;padding:1px 16px;height:1000px;">
+	<h2>Shopping Options</h2>
+	
+	<form method="post" action="shoppingcart.php">
+		<ul>
+			<li>1 silly spud        $5 <input type="button" value="add to cart"></li>
+			<li>1 round russett     $2 <input type="button" value="add to cart"></li>
+			<li>1 tremendous tater  $7 <input type="button" value="add to cart"></li>
+			<li>1 tasty tuber       $3 <input type="button" value="add to cart"></li>
+			<li>1 yucky yam         $1 <input type="button" value="add to cart"></li>	
+		</ul>
+	</form>
+	
+</div>
+
