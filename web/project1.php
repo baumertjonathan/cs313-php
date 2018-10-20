f75b4c9efe36d01c142be097782fc4d6b029853d
baumertjonathan <bau14003@byui.edu>
2018-10-20 13:39:26 -0600

    project stuff

--- web/project1.php
+++ web/project1.php
@@ -2,10 +2,19 @@
 
 try
 {
-	$user = 'postgres';
-	$password = 'password';
-	$db = new PDO('pgsql:host=localhost;dbname=myDb', $user, $password);
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $ex)
 {
