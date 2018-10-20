1c482e65bb10fbb258c76f43dfdc88c2c721e0c1
baumertjonathan <bau14003@byui.edu>
2018-10-20 12:52:17 -0600

    project

---
+++ web/project1.php
@@ -0,0 +1,20 @@
+<head>
+	<title>Project 1</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<ul>
+	<li><a href=https://mighty-journey-34749.herokuapp.com/hello.html>Home Page</a></li>
+	<li><a href=https://mighty-journey-34749.herokuapp.com/shoppingcart.php>Shopping Cart</a></li>
+	<li><a href=https://mighty-journey-34749.herokuapp.com/hello.html>...</a></li>
+	<li><a href=https://mighty-journey-34749.herokuapp.com/hello.html>...</a></li>
+	<li><a href=https://mighty-journey-34749.herokuapp.com/hello.html>...</a></li>
+</ul>
+
+<div style="margin-left:25%;padding:1px 16px;height:1000px;">
+	<h2>Project 1</h2>
+	
+	
+</div>
+</body>
