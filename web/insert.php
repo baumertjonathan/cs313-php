e2a463583510c88f415e8c4deaa6177d7ce81adc
baumertjonathan <bau14003@byui.edu>
2018-10-27 15:44:52 -0600

    dbconnect

--- web/insert.php
+++ web/insert.php
@@ -1,26 +1,6 @@
 <?php
 //connect with database
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
-
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+require('dbConnect.php');
 
 $floor = htmlspecialchars($_POST['floors']);
 $shineScore = htmlspecialchars($_POST['shineScore']);
@@ -30,8 +10,8 @@ $tileScore = htmlspecialchars($_POST['tileScore']);
 $username = htmlspecialchars($_POST['username']);
 
 
-$stmt = $db->prepare('INSERT INTO submissions(username, location, shine_score, scrff_score, dirt_score, tile_score) VALUES ($username, $floor, $shineScore, $scuffScore, $dirtScore, $tileScore);');
-$stmt = $db->prepare('INSERT INTO floors(location, shine_score, scuff_score, dirt_score, tile_score) VALUES ($floor, $shineScore, $scuffScore, $dirtScore, $tileScore);');
+//$stmt = $db->prepare('INSERT INTO submissions(username, location, shine_score, scrff_score, dirt_score, tile_score) VALUES ($username, $floor, $shineScore, $scuffScore, $dirtScore, $tileScore);');
+//$stmt = $db->prepare('INSERT INTO floors(location, shine_score, scuff_score, dirt_score, tile_score) VALUES ($floor, $shineScore, $scuffScore, $dirtScore, $tileScore);');
 
 //$stmt = $db->prepare('INSERT INTO submissions(username, location, shine_score, scrff_score, dirt_score, tile_score) VALUES (:username, :floor, :shineScore, :scuffScore, :dirtScore, :tileScore);');
 //$stmt = $db->prepare('INSERT INTO floors(location, shine_score, scuff_score, dirt_score, tile_score) VALUES (:floor, :shineScore, :scuffScore, :dirtScore, :tileScore);');
