2aff8dc0d61de9b56fed3cb20abafb474e0c5529
baumertjonathan <bau14003@byui.edu>
2018-11-02 13:17:55 -0600

    admin additions

--- web/project1Admin.php
+++ web/project1Admin.php
@@ -26,6 +26,28 @@ catch (PDOException $ex)
 
 ?>
 
+
+<script>
+function FloorsFunction() {
+    var x = document.getElementById("FloorsDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function SubmissionsFunction() {
+    var x = document.getElementById("SubmissionsDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+</script>
+
+
 <head>
 	<title>Project 1</title>
 	<link rel="stylesheet" href="styles.css">
@@ -42,32 +64,36 @@ catch (PDOException $ex)
 
 <div style="margin-left:25%;padding:1px 16px;height:1000px;">
 	<h2>Project 1</h2>
+
+ 
 <?php
 echo '<h2>Floors</h2>';
-	
+echo '<button onclick="FloorsFunction()">View</button>';
+echo '<div id="floorsDiv">';
 foreach ($db->query('SELECT location, shine_score, scuff_score, dirt_score, tile_score FROM floors') as $row)
 {
-  echo 'floor: ' . $row['location'];
+  echo ' floor: ' . $row['location'];
   echo ' shine score: ' . $row['shine_score'];
   echo ' scuff score: ' . $row['scuff_score'];
   echo ' dirt score: ' . $row['dirt_score'];
   echo ' tile score: ' . $row['tile_score'];
   echo '<br/>';
 }
-
+echo '</div>';
 echo '<h2>Submissions</h2>';
-
+echo '<button onclick="SubmissionsFunction()">View</button>';
+echo '<div id="SubmissionsDiv">';
 foreach ($db->query('SELECT username, location, shine_score, scuff_score, dirt_score, tile_score FROM submissions') as $row)
 {
 	echo 'username: ' . $row['username'];
-	echo 'floor: ' . $row['location'];
+	echo ' floor: ' . $row['location'];
  	 echo ' shine score: ' . $row['shine_score'];
 	  echo ' scuff score: ' . $row['scuff_score'];
 	  echo ' dirt score: ' . $row['dirt_score'];
  	 echo ' tile score: ' . $row['tile_score'];
 	echo '<br/>';
 }
-
+echo '</div>';
 
 ?>	
 	
