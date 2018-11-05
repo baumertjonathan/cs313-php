9e7c34ac0034d9e33814ed4de8cac8d6404fc4d8
baumertjonathan <bau14003@byui.edu>
2018-11-05 11:05:53 -0700

    stuff

--- web/insert.php
+++ web/insert.php
@@ -40,13 +40,14 @@ $dirtScore = htmlspecialchars($_POST['dirtScore']);
 $tileScore = htmlspecialchars($_POST['tileScore']);
 $username = htmlspecialchars($_POST['username']);
 
+/*
 $stmt->bindValue(':floor', $floor, PDO::PARAM_STR);
 $stmt->bindValue(':shineScore', $shineScore, PDO::PARAM_INT);
 $stmt->bindValue(':scuffScore', $scuffScore, PDO::PARAM_INT);
 $stmt->bindValue(':dirtScore', $dirtScore, PDO::PARAM_INT);
 $stmt->bindValue(':tileScore', $tileScore, PDO::PARAM_INT);
 $stmt->bindValue(':username', $username, PDO::PARAM_STR);
-
+*/
 //$stmt = $db->prepare('INSERT INTO submissions(username, location, shine_score, scuff_score, dirt_score, tile_score) VALUES (:username, :floor, :shineScore, :scuffScore, :dirtScore, :tileScore);');
 
 
